22d58019799b709c2c676c3c5ceea1fecd98b4f3
sc2646 <sc2646@cornell.edu>
2015-11-22 21:43:55 -0500

    commit

--- src/average_degree.py
+++ src/average_degree.py
@@ -1,1 +1,30 @@
 # example of program that calculates the average degree of hashtags
+import json
+def featureTwo():
+    f=open("../tweet_input/tweets.txt", "r")
+    w=open("../tweet_output/ft2.txt","wb")
+    count=0
+    for line in f:
+        #ignore lines like "{"limit": {"track":5,"timestamp_ms":"1446218985743"} }"
+        if not line.startswith('{"l'):
+            json_str=json.loads(line)
+            created_at=json_str["created_at"]
+            hashtags=json_str["entities"]["hashtags"]
+            print created_at
+            print hashtags
+            break
+            # check if the line has unicode
+            # remove or replace escape characters
+            # if(not text.encode('ascii','ignore').decode('ascii')== text):
+            #     count=count+1
+            #     text=text.encode('ascii','ignore').decode('ascii')
+            # else:
+            #     text=text.encode('ascii','ignore').decode('ascii')
+            #write cleaned text and timestamp to file
+            # w.write(text+" (timestamp:"+ created_at+")\n")
+
+    # w.write("\n")
+    # w.write(str(count)+" tweets contained unicode.")
+
+if __name__ == "__main__":
+    featureTwo()
--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -1,1 +1,25 @@
-# example of program that calculates the number of tweets cleaned
+import json
+def featureOne():
+    f=open("../tweet_input/tweets.txt", "r")
+    w=open("../tweet_output/ft1.txt","wb")
+    count=0
+    for line in f:
+        #ignore lines like "{"limit": {"track":5,"timestamp_ms":"1446218985743"} }"
+        if not line.startswith('{"l'):
+            json_str=json.loads(line)
+            created_at=json_str["created_at"]
+            text=json_str["text"]
+            # check if the line has unicode
+            # remove or replace escape characters
+            if(not text.encode('ascii','ignore').decode('ascii')== text):
+                count=count+1
+                text=text.encode('ascii','ignore').decode('ascii')
+            else:
+                text=text.encode('ascii','ignore').decode('ascii')
+            #write cleaned text and timestamp to file
+            w.write(text+" (timestamp:"+ created_at+")\n")
+    w.write("\n")
+    w.write(str(count)+" tweets contained unicode.")
+
+if __name__ == "__main__":
+    featureOne()
