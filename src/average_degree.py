10d969279bb5ffb31f9e958c72fbfe25a0e86351
sc2646 <sc2646@cornell.edu>
2015-11-25 23:22:22 -0500

    dict done

--- src/average_degree.py
+++ src/average_degree.py
@@ -1,30 +1,52 @@
 # example of program that calculates the average degree of hashtags
 import json
+import datetime
+import time
+from collections import namedtuple
+from collections import defaultdict
+
 def featureTwo():
+    tweet_dict={}
     f=open("../tweet_input/tweets.txt", "r")
     w=open("../tweet_output/ft2.txt","wb")
-    count=0
+    i=0
+    
     for line in f:
         #ignore lines like "{"limit": {"track":5,"timestamp_ms":"1446218985743"} }"
         if not line.startswith('{"l'):
             json_str=json.loads(line)
-            created_at=json_str["created_at"]
+            created_at=json_str["created_at"].encode('ascii','ignore')
             hashtags=json_str["entities"]["hashtags"]
-            print created_at
-            print hashtags
-            break
-            # check if the line has unicode
-            # remove or replace escape characters
-            # if(not text.encode('ascii','ignore').decode('ascii')== text):
-            #     count=count+1
-            #     text=text.encode('ascii','ignore').decode('ascii')
-            # else:
-            #     text=text.encode('ascii','ignore').decode('ascii')
-            #write cleaned text and timestamp to file
-            # w.write(text+" (timestamp:"+ created_at+")\n")
+            
+            ht=[]
+            if (len(hashtags)!=0):
+                for index in range (0,len(hashtags)):
+                    ht.append(hashtags[index]["text"].encode('ascii','ignore'))
+            else:
+                ht=hashtags
+            
+            i=i+1
+            
+            FMT="%H:%M:%S"
+            a=time.strptime(created_at[11:19],FMT)
+            seconds=datetime.timedelta(hours=a.tm_hour, minutes=a.tm_min, seconds=a.tm_sec).seconds
+            
+            time_stamp=namedtuple("time_stamp",["month","day","second"])
+            time_stamp_key=time_stamp(month=created_at[4:7],day=created_at[8:10],second=seconds)
+            tweet_dict.setdefault(time_stamp_key,[]).append(ht)
 
-    # w.write("\n")
-    # w.write(str(count)+" tweets contained unicode.")
+
+# if(i==32):
+#     break
+# if a tweet only has 1 or zero node then there wont be any edge
+
+
+#write cleaned text and timestamp to file
+# w.write(text+" (timestamp:"+ created_at+")\n")
+
+# w.write("\n")
+# w.write(str(count)+" tweets contained unicode.")
+print tweet_dict
 
 if __name__ == "__main__":
     featureTwo()
